1826477b737f2fac5becd68441834e60a98872b5
Alexander Sokolov <avsokol@gmail.com>
2015-07-03 15:38:12 +0300

    removed old file

--- tyndex.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'tyndex.ui'
-#
-# Created: Mon Jun 29 00:14:59 2015
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
